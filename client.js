84a7cb140a01253d55e5989cfa08ce9f18de6316
Paul Gadi <paulgadi@gmail.com>
2017-01-31 18:25:13 +0800

    changed webgl implementation from unity to babylon

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 /*  Copyright 2012-2016 Sven "underscorediscovery" Bergström
-    
+
     written by : http://underscorediscovery.ca
     written for : http://buildnewgames.com/real-time-multiplayer/
-    
+
     MIT Licensed.
 */
 
@@ -16,9 +16,10 @@ window.onload = function(){
 		//Create our game client instance.
 	game = new game_core();
 
+/*
 			//Fetch the viewport
 		game.viewport = document.getElementById('viewport');
-			
+
 			//Adjust their size
 		game.viewport.width = game.world.width;
 		game.viewport.height = game.world.height;
@@ -28,7 +29,7 @@ window.onload = function(){
 
 			//Set the draw style for the font
 		game.ctx.font = '11px "Helvetica"';
-
+*/
 		//Finally, start the loop
 	game.update( new Date().getTime() );
 
