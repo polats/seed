82eb67f267331502f25099a3bfffd71b1b19f392
Paul Gadi <paulgadi@gmail.com>
2017-02-07 12:21:32 +0800

    added express and index.html

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
+var
+  port            = process.env.PORT || 3000,
 
-var port = process.env.PORT || 3000;
+  io              = require('socket.io'),
+  express         = require('express'),
+  path            = require('path'),  
+  http            = require('http'),
+  app             = express(),
+  server          = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 server.listen(port);
+var io = io.listen(server);
+
 console.log('Server started on port', port);
+
+//By default, we forward the / path to index.html automatically.
+app.get( '/', function( req, res ){
+console.log('trying to load %s', __dirname + '/index.html');
+res.sendfile( '/index.html' , { root:__dirname });
+});
 
 io.on('connection', function (socket) {
   console.log('Connection received');
