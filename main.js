c827479c8d975d04c8b73eccd2a1997e32cc54e3
Paul Gadi <paulgadi@gmail.com>
2017-03-24 21:01:25 +0800

    run both incheon and proxy on server on different ports

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-var fs = require('fs'),
-    dotenv = require('dotenv'),
-    ProxyControlsServer = require('./server/proxy-controls-server');
-
-dotenv.config({silent: true});
-
-if (process.env.SSL_PORT) {
-  new ProxyControlsServer({
-    port: process.env.PORT,
-    sslPort: process.env.SSL_PORT,
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-  });
-} else {
-  new ProxyControlsServer({
-    port: process.env.PORT || process.env.npm_package_config_port
-  });
-}
-
-
-
-/*
 'use strict';
 
 const express = require('express');
@@ -50,4 +28,21 @@ const serverEngine = new SLServerEngine(io, gameEngine, { debug: {}, updateRate:
 // start the game
 serverEngine.start();
 
-*/
+var fs = require('fs'),
+    dotenv = require('dotenv'),
+    ProxyControlsServer = require('./server/proxy-controls-server');
+
+dotenv.config({silent: true});
+
+if (process.env.SSL_PORT) {
+  new ProxyControlsServer({
+    port: process.env.PORT,
+    sslPort: process.env.SSL_PORT,
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+  });
+} else {
+  new ProxyControlsServer({
+    port: process.env.PORT || process.env.npm_package_config_port
+  });
+}
