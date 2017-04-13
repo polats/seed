06429905642239217b0835d8ebcfaf84f212e38d
Paul Gadi <paulgadi@gmail.com>
2017-04-13 17:26:41 +0800

    limit player movement

--- src/common/CarControl.js
+++ src/common/CarControl.js
@@ -1,10 +1,11 @@
 'use strict';
 const FORWARD_IMPULSE = 0.45;
-const MAX_VELOCITY = 1000;
+const MAX_VELOCITY = 30;
 const MIN_TURNING_VELOCITY = 4.0;
 const TURN_IMPULSE = 0.14;
 const SMALL_TURNING_VELOCITY = 8.0;
 const BOOST_VELOCITY = 3.0;
+const DASH_MODIFIER = 0.8;
 let CANNON = null;
 
 class CarControl {
@@ -54,14 +55,36 @@ class CarControl {
 
     dash(car, inputData)
     {
+      /*
       let impulse = inputData.options.dashSpeed;
       let newVec = car.physicsObj.quaternion.vmult(
         new CANNON.Vec3(
           inputData.options.dashVector.x,
           inputData.options.dashVector.z,
           impulse));
+      */
+
+      inputData.options.dashVector.x = inputData.options.dashVector.x * DASH_MODIFIER;
+      inputData.options.dashVector.y = inputData.options.dashVector.y * DASH_MODIFIER;
+      inputData.options.dashVector.z = inputData.options.dashVector.z * DASH_MODIFIER;
+
+      let newSpeedVec = car.physicsObj.velocity.clone();
+      newSpeedVec.vadd(inputData.options.dashVector, newSpeedVec);
+      let newSpeedLen = newSpeedVec.length();
+
+      let velocityRatio = (newSpeedLen - MAX_VELOCITY) / MAX_VELOCITY;
+
+      if (velocityRatio > 0)
+      {
+        let multiplier = 1 - velocityRatio;
+
+        inputData.options.dashVector.x = inputData.options.dashVector.x * multiplier;
+        inputData.options.dashVector.y = inputData.options.dashVector.y * multiplier;
+        inputData.options.dashVector.z = inputData.options.dashVector.z * multiplier;
+      }
 
       car.physicsObj.velocity.vadd(inputData.options.dashVector, car.physicsObj.velocity);
+
     }
 }
 
